9a2a24c38c1539d4cabccf29a4ce557b65a256bf
bc299 <bc299@duke.edu>
2023-09-23 14:28:24 -0400

    added parametrize test case

--- dbdpy/device.py
+++ dbdpy/device.py
@@ -6,5 +6,11 @@ class CommercialDevice:
     def get_device_info(self):
         return f"{self.brand} - {self.model}"
 
-    def whatthe(self):
-        print("Whathe")
+    def calculate_rescale_value(self, age, is_damaged):
+        initial_value = 1000
+        depre_rate = 0.20 if self.brand == "Apple" else 0.15
+        rescale_value = initial_value * ((1 - depre_rate) ** age)
+        if is_damaged:
+            rescale_value *= 0.70
+
+        return round(rescale_value, 2)
--- tests/test_device.py
+++ tests/test_device.py
@@ -1,3 +1,4 @@
+import pytest
 from dbdpy import device
 
 
@@ -6,6 +7,14 @@ def test_get_device_info():
     assert dev.get_device_info() == "Apple Watch - Series 9"
 
 
-def test_ha_device_info():
-    dev = device.CommercialDevice("Apple Watch", "Series 9")
-    assert dev.get_device_info() == "Apple Watch - Series 9"
+@pytest.mark.parametrize(
+    "brand, age, is_damaged, expected",
+    [
+        ("Apple", 1, False, 800),
+        ("Apple", 2, False, 640),
+        ("Fitbit", 1, False, 850),
+    ],
+)
+def test_calculate_rescale_value(brand, age, is_damaged, expected):
+    dev = device.CommercialDevice(brand, "AnyModel")
+    assert dev.calculate_rescale_value(age, is_damaged) == expected
