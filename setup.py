dd8dec2fedcd464fc6bb2d760e82b1ec215976f3
bc299 <bc299@duke.edu>
2023-09-23 13:46:23 -0400

    added some basic setup information to the setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+DESCRIPTION = "Python package for preprocessing and analyzing wearable device data"
+
+setup(
+    name="dbdpy",
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[]
+)
