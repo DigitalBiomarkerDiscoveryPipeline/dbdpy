f530b0e8f59678e5cb1a4ba62ac045a5897d2fbd
bc299 <bc299@duke.edu>
2023-09-25 18:18:55 -0400

    commented out code for demo purposes

--- tests/test_device.py
+++ tests/test_device.py
@@ -7,14 +7,14 @@ def test_get_device_info():
     assert dev.get_device_info() == "Apple Watch - Series 9"
 
 
-@pytest.mark.parametrize(
-    "brand, age, is_damaged, expected",
-    [
-        ("Apple", 1, False, 800),
-        ("Apple", 2, False, 640),
-        ("Fitbit", 1, False, 850),
-    ],
-)
-def test_calculate_rescale_value(brand, age, is_damaged, expected):
-    dev = device.CommercialDevice(brand, "AnyModel")
-    assert dev.calculate_rescale_value(age, is_damaged) == expected
+# @pytest.mark.parametrize(
+#     "brand, age, is_damaged, expected",
+#     [
+#         ("Apple", 1, False, 800),
+#         ("Apple", 2, False, 640),
+#         ("Fitbit", 1, False, 850),
+#     ],
+# )
+# def test_calculate_rescale_value(brand, age, is_damaged, expected):
+#     dev = device.CommercialDevice(brand, "AnyModel")
+#     assert dev.calculate_rescale_value(age, is_damaged) == expected
