58ac0b52666f0017aaf07b84905dac1aa53daf9b
bc299 <bc299@duke.edu>
2024-01-05 14:05:24 -0500

    fixed formatting issues with black

--- dbdpy/apple_watch.py
+++ dbdpy/apple_watch.py
@@ -9,16 +9,16 @@ from dbdpy.commercial_device import CommercialDevice
 class AppleWatch(CommercialDevice):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-    
+
     @classmethod
-    def read_file(cls, filepath: str, device_name: str="Apple Watch"):
+    def read_file(cls, filepath: str, device_name: str = "Apple Watch"):
         """
-        Reads health data from an XML file specific to an Apple Watch and initializes 
+        Reads health data from an XML file specific to an Apple Watch and initializes
         an instance of the `AppleWatch` class with this data.
 
-        This method parses the XML file to extract various health metrics such as 
-        energy burned, step count, distance walked or run, oxygen saturation, and heart rate. 
-        It then filters and formats the data before initializing an `AppleWatch` object with 
+        This method parses the XML file to extract various health metrics such as
+        energy burned, step count, distance walked or run, oxygen saturation, and heart rate.
+        It then filters and formats the data before initializing an `AppleWatch` object with
         the extracted information.
 
         Parameters
@@ -31,16 +31,16 @@ class AppleWatch(CommercialDevice):
         Returns
         -------
         AppleWatch
-            An instance of the `AppleWatch` class, initialized with health data extracted 
+            An instance of the `AppleWatch` class, initialized with health data extracted
             from the XML file.
 
         Notes
         -----
-        - The method assumes that the XML file is structured in a specific format compatible 
+        - The method assumes that the XML file is structured in a specific format compatible
           with Apple Watch data.
-        - Health data is extracted based on predefined health metric types and may not include 
+        - Health data is extracted based on predefined health metric types and may not include
           all data present in the XML file.
-        - The device name is used to filter out data from sources other than the specified 
+        - The device name is used to filter out data from sources other than the specified
           Apple Watch.
 
         Examples
@@ -56,7 +56,9 @@ class AppleWatch(CommercialDevice):
         record_df = pd.DataFrame(record_list)
 
         # Only save the watch data and discard the iPhone data
-        record_df["sourceName"] = record_df["sourceName"].apply(lambda x: unicodedata.normalize("NFKC", x))
+        record_df["sourceName"] = record_df["sourceName"].apply(
+            lambda x: unicodedata.normalize("NFKC", x)
+        )
         record_df = record_df[record_df["sourceName"].str.contains(device_name)]
 
         # Drop columns that will not be used
@@ -65,30 +67,54 @@ class AppleWatch(CommercialDevice):
 
         # Convert datetime to ISO 8601 format
         datetime_cols = ["startDate", "endDate"]
-        record_df[datetime_cols] = record_df[datetime_cols]\
-            .apply(lambda x: pd.to_datetime(x).dt.strftime("%Y-%m-%dT%H:%M:%S"))
-        
+        record_df[datetime_cols] = record_df[datetime_cols].apply(
+            lambda x: pd.to_datetime(x).dt.strftime("%Y-%m-%dT%H:%M:%S")
+        )
+
         # Convert values to numeric type
         record_df["value"] = pd.to_numeric(record_df["value"], errors="coerce")
-        
+
         # Shorten observation names
-        record_df["type"] = record_df["type"].str.replace("HKQuantityTypeIdentifier", "")
-        record_df["type"] = record_df["type"].str.replace("HKCategoryTypeIdentifier", "")
+        record_df["type"] = record_df["type"].str.replace(
+            "HKQuantityTypeIdentifier", ""
+        )
+        record_df["type"] = record_df["type"].str.replace(
+            "HKCategoryTypeIdentifier", ""
+        )
 
         # Extract data, convert to standard format, and populate attributes
-        energy = record_df[record_df["type"] == "BasalEnergyBurned"]\
-            .drop(columns="type").rename(columns={"value": "active_calories"})
-        steps = record_df[record_df["type"] == "StepCount"]\
-            .drop(columns="type").rename(columns={"value": "steps"})
-        distance = record_df[record_df["type"] == "DistanceWalkingRunning"]\
-            .drop(columns="type").rename(columns={"value": "distance"})
-        oxygen = record_df[record_df["type"] == "OxygenSaturation"]\
-            .drop(columns="type").rename(columns={"value": "SpO2"})
-        heart_rate = record_df[record_df["type"] == "HeartRate"]\
-            .drop(columns="type").rename(columns={"value": "heart_rate"})
-        sleep = record_df[record_df["type"] == "SleepAnalysis"]\
+        energy = (
+            record_df[record_df["type"] == "BasalEnergyBurned"]
             .drop(columns="type")
+            .rename(columns={"value": "active_calories"})
+        )
+        steps = (
+            record_df[record_df["type"] == "StepCount"]
+            .drop(columns="type")
+            .rename(columns={"value": "steps"})
+        )
+        distance = (
+            record_df[record_df["type"] == "DistanceWalkingRunning"]
+            .drop(columns="type")
+            .rename(columns={"value": "distance"})
+        )
+        oxygen = (
+            record_df[record_df["type"] == "OxygenSaturation"]
+            .drop(columns="type")
+            .rename(columns={"value": "SpO2"})
+        )
+        heart_rate = (
+            record_df[record_df["type"] == "HeartRate"]
+            .drop(columns="type")
+            .rename(columns={"value": "heart_rate"})
+        )
+        sleep = record_df[record_df["type"] == "SleepAnalysis"].drop(columns="type")
 
-        return cls(sleep=sleep, energy=energy, steps=steps, distance=distance, oxygen=oxygen,
-                   heart_rate=heart_rate)
-    
+        return cls(
+            sleep=sleep,
+            energy=energy,
+            steps=steps,
+            distance=distance,
+            oxygen=oxygen,
+            heart_rate=heart_rate,
+        )
--- dbdpy/commercial_device.py
+++ dbdpy/commercial_device.py
@@ -1,15 +1,16 @@
 class CommercialDevice:
-    def __init__(self, 
-                 sleep=None, 
-                 energy=None, 
-                 steps=None, 
-                 distance=None, 
-                 oxygen=None, 
-                 heart_rate=None) -> None:
+    def __init__(
+        self,
+        sleep=None,
+        energy=None,
+        steps=None,
+        distance=None,
+        oxygen=None,
+        heart_rate=None,
+    ) -> None:
         self.sleep = sleep
         self.energy = energy
         self.steps = steps
         self.distance = distance
         self.oxygen = oxygen
         self.heart_rate = heart_rate
-    
